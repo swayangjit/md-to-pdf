827b4bb56474dc61863e30111f6f8d8a64d73ed2
swayangjit <swayangjit029@gmail.com>
2025-04-07 00:01:34 +0530

    Issue #MTP-000 feat: Updated mdtoPDF library

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // index.js
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
-// const puppeteer = require('puppeteer-core');
-const marked = require('marked');
-// const chromium = require('@sparticuz/chromium')
 const { mdToPdf } = require('md-to-pdf');
 require('dotenv').config();
 
@@ -21,95 +18,17 @@ app.use(express.json());
 
 app.post('/generatePdf', async (req, res) => {
     const { markdown } = req.body;
-    const htmlContent = marked.parse(markdown);
-    const html1 = `<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>PDF Template</title>
-    
-    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-    <style>
-    @page {
-            size: A4;
-            margin-top: 10mm;
-            margin-bottom: 20mm;
-            margin-left: 10mm;
-            margin-right: 10mm;
-        }
-    body  {
-        margin: 0;
-        padding: 2rem;
-        font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI Emoji', 'Segoe UI Symbol', 'Apple Color Emoji', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        background-color: #ffffff;
-        color: #333;
-        font-size: 16px;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
-         h1, h2, h3, h4, h5, h6 {
-      font-weight: 600;
-      margin-top: 1.5em;
-      margin-bottom: 0.5em;
-    }
-
-    /* Paragraphs */
-    p {
-      margin: 1em 0;
-    }
-        .header {
-            text-align: center;
-            padding: 10px 0;
-            border-bottom: 1px solid #000;
-        }
-        .header img {
-            max-width: 150px;
-        }
-        .content {
-            margin-top: 20px;
-            font-size: 12px;
-        }
-    </style>
-</head>
-<body>
-    <div class="header">
-        <img src="https://tkhqppfqsitovjvsstfl.supabase.co/storage/v1/object/public/assets/netskillLogo.png" alt="Company Logo">
-    </div>
-    <div class="content">
-        ${htmlContent}
-</body>
-</html>
-`
+   
     const headerHtml = `<p align="center">
   <img src="https://tkhqppfqsitovjvsstfl.supabase.co/storage/v1/object/public/assets/netskillLogo.png" alt="Header Image" width="300" height="50"/>
 <hr style="border: none; height: .5px; background-color: #ccc; <br>" />
 
 `;
-    console.log('Formatted HTML', html1);
     if (!markdown) {
         return res.status(400).json({ error: 'MD content is required' });
     }
 
     try {
-        // const browser = await puppeteer.launch();
-        // browser = await puppeteer.launch({
-        //     args: chromium.args,
-        //     defaultViewport: chromium.defaultViewport,
-        //     executablePath: await chromium.executablePath(),
-        //     headless: chromium.headless,
-        //     ignoreHTTPSErrors: true,
-        // });
-        // const page = await browser.newPage();
-        // await page.setContent(html1, { waitUntil: 'load' });
-
-        // const pdfBuffer = await page.pdf({
-        //     format: 'A4',
-        //     printBackground: true,
-        //     margin: { top: '10mm', bottom: '20mm', left: '1mm', right: '1mm' }
-        // });
-
-        // await browser.close();
-
         const pdf = await mdToPdf({ content: headerHtml + markdown });
 
         // Upload to Supabase
